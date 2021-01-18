83926376551a9b189cd93aa860ac981912845f5d
Alicia Vega <aliciavega@Alicias-MacBook-Pro.local>
2021-01-17 20:38:38 -0700

    collected info and save to local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,22 @@
-// Current time at the top of the page
-$(document).ready(function(){
-  $('#currentDay').text(moment().format("dddd, MMMM YYYY h:mm a"));
+$(document).ready(function () {
+  // Current day at the top of the page
+  $("#currentDay").text(moment().format("dddd, MMMM Do, YYYY h:mm a"));
+
+  // TA office hours notes. Set item to local storage
+  $(".saveBtn").on("click", function() {
+    var userInput = $(this)
+      .siblings(".description")
+      .val(); // Made variable to serve as value in localstorage
+    var hourBlock = $(this)
+      .siblings(".description")
+      .attr("id"); // Key should show the id of the block
+    
+    localStorage.setItem(hourBlock, userInput);
+  });
+
+  // Get items from localstorage
+  
+
+
+
 });
